dacaccacbc9be44c49df02b30a95ef2006ddc3c5
Ken Graf <kengraf57@gmail.com>
2023-01-28 22:32:16 -0500

    Update http.go

--- http.go
+++ http.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+    "net"
     "fmt"
     "net/http"
     "math/rand"
     "time"
 )
 
+// GetLocalIP returns the non loopback local IP of the host
+func GetLocalIP() string {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return ""
+    }
+    for _, address := range addrs {
+        // check the address type and if it is not a loopback the display it
+        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                return ipnet.IP.String()
+            }
+        }
+    }
+    return ""
+}
+
 func hello(w http.ResponseWriter, req *http.Request) {
 
     time.Sleep( time.Duration(rand.Intn(10)) * time.Second)
-    fmt.Fprintf(w, "hello\n")
+    fmt.Fprintf(w, "hello from: "+GetLocalIP() )
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
